c3abfebdbff9d3df8bb841bcaaba11a5ab345ef9
Bruno Nascimento <sputnicker@gmail.com>
2021-11-23 19:04:58 -0300

    added sections to index page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+
+import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import Info from '../components/Info'
+import Contact from '../components/Contact'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>Meu Freela</title>
         <meta
@@ -14,9 +19,28 @@ export default function Home() {
         <link rel='icon' href='/favicon.png' />
       </Head>
 
-      <main className={styles.main}>
-        Hello world!!!
-      </main>
-    </div>
+      <Navbar />
+
+      <Hero>
+        <h2>Desenvolvedores prontos para ação</h2>
+        <h3>
+          Seu projeto pronto <wbr />
+          na velocidade da luz
+        </h3>
+        <a href="#Contato">Peça uma reunião</a>
+      </Hero>
+
+      <Info>
+        <h3>Nossa iniciativa</h3>
+        <p>
+          Somos desenvolvedores que se dedicam a fazer projetos perfeitos e
+          entregar valor para todos os seus clientes. Gostamos de ver clientes
+          satisfeitos com projetos feitos com carinho, atenção e qualidade
+          altíssima.
+        </p>
+      </Info>
+      <Contact/>
+      <Footer/>
+    </>
   )
 }
