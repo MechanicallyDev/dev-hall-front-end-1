7c70adcd9bb521f716f28259e996080eb6ed48da
Bruno Nascimento <sputnicker@gmail.com>
2021-11-23 19:04:36 -0300

    added footer

---
+++ components/Footer.js
@@ -0,0 +1,10 @@
+import styles from './Footer.module.css'
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>Desafio 1 - Front-end iniciante</p>
+      <p>Iniciativa <strong>DEV HALL</strong></p>
+    </footer>
+  )
+}
